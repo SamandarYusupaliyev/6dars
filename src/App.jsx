e5b39feab052b388aebb3e4dbcf2e8e2571d43c7
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-04-04 20:19:51 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+    return(
+      <div>
+          <h1>Hello</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde totam corrupti illum error voluptatem, nihil vel laudantium porro sint ad iure dolores? Quas fugiat provident totam, eum quaerat delectus dicta.</p>
+           <button className="btn btn-primary">click</button>
+      </div>
+    )
+}
+export default App
